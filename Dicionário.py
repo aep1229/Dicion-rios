a9f278d3acf39e679f4c896f99080b274a645b6c
aep1229 <136847393+aep1229@users.noreply.github.com>
2023-06-23 15:56:55 -0300

    Add files via upload

---
+++ Dicionário.py
@@ -0,0 +1,24 @@
+"""
+Programa dicionário
+Descrição: Este programa apresenta a estrutura de dados dicionário em Python para fins de aprendizado
+Data: 23/06/23
+Autora:
+Versão: 0.0.1
+"""
+
+# Atribuição de variáveis
+
+tabela = {"Arroz" : 0.5, "Batata" : 0.2, "Carne" : 0.1}
+
+# Entrada de dados
+
+# Processamento de dados
+
+# Saída de dados
+
+# print(tabela["Arroz"])
+
+# print(tabela{"Alface"})
+
+ print(tabela["Alface"])
+
